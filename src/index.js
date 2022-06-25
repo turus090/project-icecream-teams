e123c4a60e68db4b9c087345a0a963bd888bc720
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-06-26 01:23:14 +0300

    модалка
    еще не закончил

--- src/index.js
+++ src/index.js
@@ -45,3 +45,27 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+// модальное окно в contacts
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");
+
+  }
+
+}
+)();
