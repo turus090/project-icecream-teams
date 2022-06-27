1c0bad289584b28962e513ec2134223635c1d7d4
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-06-27 22:34:17 +0300

    maps
    не получается подключить

--- src/maps.js
+++ src/maps.js
@@ -2,7 +2,7 @@
     const refs = {
       openModalBtn: document.querySelector("[data-maps-open]"),
       closeModalBtn: document.querySelector("[data-maps-close]"),
-      maps: document.querySelector("[maps-modal]"),
+    modal: document.querySelector("[maps-modal]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
